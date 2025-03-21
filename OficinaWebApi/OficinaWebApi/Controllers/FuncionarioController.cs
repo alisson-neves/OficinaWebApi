e6e180fad9e83c29834c57e3bf09dbbe9b25d46d
Alisson Neves <alissondasneves@hotmail.com>
2025-03-20 21:55:06 -0300

    Add files via upload

---
+++ OficinaWebApi/OficinaWebApi/Controllers/FuncionarioController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OficinaWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FuncionarioController : ControllerBase
+    {
+        static List<string> funcionarios = new List<string>();
+
+        public DbContexto context { get; set; }
+
+        public FuncionarioController(DbContexto dbContext) 
+        { 
+            context = dbContext;
+        }
+
+        [HttpGet]
+        public ActionResult Get() 
+        {
+            //return Ok(funcionarios.ToList());
+            return Ok(context.Employee.ToList());
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult Get(int id)
+        {
+            var funcionario = context.Employee.FirstOrDefault(x => x.Id == id);
+
+            if (funcionario == null)
+            {
+                return BadRequest("Usuário não encontrado.");
+            }
+            //return Ok(funcionarios.ToList());
+            return Ok(funcionario);
+        }
+
+        [HttpPost]
+        public ActionResult Post(Employee funcionario)
+        {
+            
+            context.Employee.Add(funcionario);
+            context.SaveChanges();
+           // funcionarios.Add(funcionario);
+            return Ok("Funcionário adicionado com sucesso!");
+        }
+
+        [HttpPut]
+        public ActionResult Put(string nome)
+        {
+            var funcionario = funcionarios.FirstOrDefault(x => x == nome);
+            if (funcionario == null)
+            {
+                return BadRequest("Funcionário não encontrado.");
+            }
+
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            var funcionario = context.Employee.FirstOrDefault(x => x.Id == id);
+
+            if (funcionario == null) 
+            {
+                return BadRequest("Usuário não encontrado.");
+            }
+
+            context.Employee.Remove(funcionario);
+            context.SaveChanges();
+            return Ok("Funcionário removido!");
+        }
+
+    }
+}
---
+++ OficinaWebApi/OficinaWebApi/DbContexto.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace OficinaWebApi
+{
+    public class DbContexto : DbContext 
+    {
+        public DbSet<Employee> Employee { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseNpgsql("User ID=webapioficina;Password=j9tqiqNMRxcWuMpb7bJ6rUxKohSoZMZi;Host=dpg-cvd0a7pc1ekc73aspdq0-a.oregon-postgres.render.com;Port=5432;Database=webapioficina;Pooling=true;");        
+        }
+    }
+}
